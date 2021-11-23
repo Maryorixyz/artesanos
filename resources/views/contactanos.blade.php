bdb09831f3f79017b056f98ebac129190a658b2b
maryorixyz <maryoriaraujosanti@gmail.com>
2021-11-23 04:12:34 -0500

    modificacion de las views
    - nosotros
    - contactanos
    - agregamos @extends para el footer

--- resources/views/contactanos.blade.php
+++ resources/views/contactanos.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Contact</title>
+	<title>Contactos</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -297,20 +297,20 @@
 				<div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
 					<form>
 						<h4 class="mtext-105 cl2 txt-center p-b-30">
-							Send Us A Message
+							Envianos un Mensaje
 						</h4>
 
 						<div class="bor8 m-b-20 how-pos4-parent">
-							<input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Your Email Address">
+							<input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="email" placeholder="Tu correo Electronico">
 							<img class="how-pos4 pointer-none" src="images/icons/icon-email.png" alt="ICON">
 						</div>
 
 						<div class="bor8 m-b-30">
-							<textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="How Can We Help?"></textarea>
+							<textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="¿Como podemos ayudarte?"></textarea>
 						</div>
 
 						<button class="flex-c-m stext-101 cl0 size-121 bg3 bor1 hov-btn3 p-lr-15 trans-04 pointer">
-							Submit
+							Enviar
 						</button>
 					</form>
 				</div>
@@ -323,11 +323,11 @@
 
 						<div class="size-212 p-t-2">
 							<span class="mtext-110 cl2">
-								Address
+								Ubicanos
 							</span>
 
 							<p class="stext-115 cl6 size-213 p-t-18">
-								Coza Store Center 8th floor, 379 Hudson St, New York, NY 10018 US
+								Jr.Huallaga 300, Amarilis 10002
 							</p>
 						</div>
 					</div>
@@ -339,11 +339,11 @@
 
 						<div class="size-212 p-t-2">
 							<span class="mtext-110 cl2">
-								Lets Talk
+								Llamanos
 							</span>
 
 							<p class="stext-115 cl1 size-213 p-t-18">
-								+1 800 1236879
+								999999999
 							</p>
 						</div>
 					</div>
@@ -355,11 +355,11 @@
 
 						<div class="size-212 p-t-2">
 							<span class="mtext-110 cl2">
-								Sale Support
+								Contactanos
 							</span>
 
 							<p class="stext-115 cl1 size-213 p-t-18">
-								contact@example.com
+								prueba@gmail.com
 							</p>
 						</div>
 					</div>
@@ -367,110 +367,10 @@
 			</div>
 		</div>
 	</section>	
-	
-	
-	<!-- Map -->
-	{{-- <div class="map">
-		<div class="size-303" id="google_map" data-map-x="40.691446" data-map-y="-73.886787" data-pin="images/icons/pin.png" data-scrollwhell="0" data-draggable="1" data-zoom="11"></div>
-	</div> --}}
-
-
 
 	<!-- Footer -->
-	<footer class="bg3 p-t-75 p-b-32">
-		<div class="container">
-			<div class="row justify-content-md-center">
-				<div class="col-sm-6 col-lg-3 p-b-50">
-					<svg id="R_copia" data-name="R copia" xmlns="http://www.w3.org/2000/svg" width="100" height="60" viewBox="0 0 200 200" style="max-height: 60px;">
-						<path id="Forma_3" data-name="Forma 3" class="cls-1" d="M23.99,108.99l35.843,47.019h21.18L43.54,108.99"/>
-						<path id="Forma_1_copia" data-name="Forma 1 copia" class="cls-1" d="M20.143,157L4,147.5V92.071a8.09,8.09,0,0,1,8.071-4.751,7.979,7.979,0,0,1,6.457,4.751v4.751H50.814a23.41,23.41,0,0,0,0-38.007H18.529v9.5a9.862,9.862,0,0,1-6.457,3.167A9.977,9.977,0,0,1,4,68.3V44.562H55.657A37.533,37.533,0,0,1,75.028,74.651c0.9,13.983-6.547,27.56-19.371,34.84H20.143V157Z"/>
-						<path id="Forma_6" data-name="Forma 6" class="cls-1" d="M114.027,126.505a6.091,6.091,0,0,0-2.886,5.622,5.968,5.968,0,0,0,2.886,4.819h21.642a6.7,6.7,0,0,0-.721-10.441H114.027Z"/>
-						<path id="Forma_4" data-name="Forma 4" class="cls-1" d="M60,156L112.024,43.008q24.2,50.488,48.4,100.975l16.615-.814L140.92,72.323l-2.889,5.7a6.63,6.63,0,0,1-7.225-.814,8.788,8.788,0,0,1-2.889-8.143l13-26.058L198,156H152q-19.987-41.838-39.976-83.677L79.516,143.169H90.352q10.475-20.358,20.95-40.716a6.557,6.557,0,0,1,7.224,0,8.461,8.461,0,0,1,3.612,7.329L98,156H60Z"/>
-					</svg>
-					<h4 class="stext-301 cl0 p-b-30">
-						Sobre Nosotros
-					</h4>
-					<p class="stext-107 cl7 size-201 text-justify">
-						RUTA ARTESANAL AMARILIS, tiene como objetivo acercar a la asociacion de artesanos tradicionales a las posibilidades que ofrece la tecnología para la promoción de sus productos y para su inclusión socioeconómica.
-					</p>
-				</div>
-				<div class="col-sm-6 col-lg-2 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Categorias
-					</h4>
-					<ul>
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Mascaras
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Traje
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Manualidades
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div class="col-sm-6 col-lg-3 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Ubicación
-					</h4>
-					<ul>
-						<li class="p-b-10">
-							<span class="fa fa-map-marker fs-18 cl7 m-r-16"></span>
-							<span class="stext-107 cl7 size-201"> Jr.Huallaga 300, Amarilis 10002 </span>
-						</li>
-						<li class="p-b-10">
-							<span class="fa fa-phone fs-18 cl7 m-r-16"></span>
-							<span class="stext-107 cl7 size-201">999999999</span>
-						</li>
-
-						<li class="p-b-10">
-							<span class="fa fa-envelope fs-18 cl7 m-r-16"></span>
-							<span class="stext-107 cl7 size-201">prueba@gmail.com</span>
-						</li>
-					</ul>					
-				</div>
-
-				<div class="col-sm-6 col-lg-2 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Contactanos
-					</h4>
-
-					<form>
-						<div class="wrap-input1 w-full p-b-4">
-							<input class="input1 bg-none plh1 stext-107 cl7" type="text" name="email" placeholder="email@example.com">
-							<div class="focus-input1 trans-04"></div>
-						</div>
-
-						<div class="p-t-18">
-							<button class="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
-								Subscribe
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
-
-			{{-- <div class="p-t-40">
-				<p class="stext-107 cl6 txt-center">
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
-				</p>
-			</div> --}}
-		</div>
-	</footer>
-
-
+	@extends('footer')
+	
 	<!-- Back to top -->
 	<div class="btn-back-to-top" id="myBtn">
 		<span class="symbol-btn-back-to-top">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -295,10 +295,6 @@
 								<span class="block1-name ltext-102 trans-04 p-b-8">
 									Asociacion
 								</span>
-
-								<span class="block1-info stext-102 trans-04">
-									Spring 2018
-								</span>
 							</div>
 
 							<div class="block1-txt-child2 p-b-4 trans-05">
@@ -605,128 +601,7 @@
 
 
 	<!-- Footer -->
-	<footer class="bg3 p-t-75 p-b-32">
-		<div class="container">
-			<div class="row justify-content-md-center">
-				<div class="col-sm-6 col-lg-3 p-b-50">
-					<svg id="R_copia" data-name="R copia" xmlns="http://www.w3.org/2000/svg" width="100" height="60" viewBox="0 0 200 200" style="max-height: 60px;">
-						<path id="Forma_3" data-name="Forma 3" class="cls-1" d="M23.99,108.99l35.843,47.019h21.18L43.54,108.99"/>
-						<path id="Forma_1_copia" data-name="Forma 1 copia" class="cls-1" d="M20.143,157L4,147.5V92.071a8.09,8.09,0,0,1,8.071-4.751,7.979,7.979,0,0,1,6.457,4.751v4.751H50.814a23.41,23.41,0,0,0,0-38.007H18.529v9.5a9.862,9.862,0,0,1-6.457,3.167A9.977,9.977,0,0,1,4,68.3V44.562H55.657A37.533,37.533,0,0,1,75.028,74.651c0.9,13.983-6.547,27.56-19.371,34.84H20.143V157Z"/>
-						<path id="Forma_6" data-name="Forma 6" class="cls-1" d="M114.027,126.505a6.091,6.091,0,0,0-2.886,5.622,5.968,5.968,0,0,0,2.886,4.819h21.642a6.7,6.7,0,0,0-.721-10.441H114.027Z"/>
-						<path id="Forma_4" data-name="Forma 4" class="cls-1" d="M60,156L112.024,43.008q24.2,50.488,48.4,100.975l16.615-.814L140.92,72.323l-2.889,5.7a6.63,6.63,0,0,1-7.225-.814,8.788,8.788,0,0,1-2.889-8.143l13-26.058L198,156H152q-19.987-41.838-39.976-83.677L79.516,143.169H90.352q10.475-20.358,20.95-40.716a6.557,6.557,0,0,1,7.224,0,8.461,8.461,0,0,1,3.612,7.329L98,156H60Z"/>
-					</svg>
-					<h4 class="stext-301 cl0 p-b-30">
-						Sobre Nosotros
-					</h4>
-					<p class="stext-107 cl7 size-201 text-justify">
-						RUTA ARTESANAL AMARILIS, tiene como objetivo acercar a la asociacion de artesanos tradicionales a las posibilidades que ofrece la tecnología para la promoción de sus productos y para su inclusión socioeconómica.
-					</p>
-				</div>
-				<div class="col-sm-6 col-lg-2 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Categorias
-					</h4>
-					<ul>
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Mascaras
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Traje
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Manualidades
-							</a>
-						</li>
-
-						{{-- <li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Watches
-							</a>
-						</li>
-					</ul> --}}
-				</div>
-				<div class="col-sm-6 col-lg-3 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Ubicación
-					</h4>
-					<ul>
-						<li class="p-b-10">
-							<span class="fa fa-map-marker fs-18 cl7 m-r-16"></span>
-							<span class="stext-107 cl7 size-201"> Jr.Huallaga 300, Amarilis 10002 </span>
-						</li>
-						<li class="p-b-10">
-							<span class="fa fa-phone fs-18 cl7 m-r-16"></span>
-							<span class="stext-107 cl7 size-201">999999999</span>
-						</li>
-
-						<li class="p-b-10">
-							<span class="fa fa-envelope fs-18 cl7 m-r-16"></span>
-							<span class="stext-107 cl7 size-201">prueba@gmail.com</span>
-						</li>
-					</ul>					
-				</div>
-
-				{{-- <div class="col-sm-6 col-lg-3 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						GET IN TOUCH
-					</h4>
-
-					<p class="stext-107 cl7 size-201">
-						Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
-					</p>
-
-					<div class="p-t-27">
-						<a href="#" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-							<i class="fa fa-facebook"></i>
-						</a>
-
-						<a href="#" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-							<i class="fa fa-instagram"></i>
-						</a>
-
-						<a href="#" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
-							<i class="fa fa-pinterest-p"></i>
-						</a>
-					</div>
-				</div> --}}
-
-				<div class="col-sm-6 col-lg-2 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Contactanos
-					</h4>
-
-					<form>
-						<div class="wrap-input1 w-full p-b-4">
-							<input class="input1 bg-none plh1 stext-107 cl7" type="text" name="email" placeholder="email@example.com">
-							<div class="focus-input1 trans-04"></div>
-						</div>
-
-						<div class="p-t-18">
-							<button class="flex-c-m stext-101 cl0 size-103 bg1 bor1 hov-btn2 p-lr-15 trans-04">
-								Subscribe
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
-
-			{{-- <div class="p-t-40">
-				<p class="stext-107 cl6 txt-center">
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
-				</p>
-			</div> --}}
-		</div>
-	</footer>
+	@extends('footer')
 
 
 	<!-- Back to top -->
